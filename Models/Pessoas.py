490b45ce26a8c0b26b5145ef1c632731d41faf69
EAn022 <162371161+EAn022@users.noreply.github.com>
2025-05-14 17:11:09 +0000

    fix: pessoas

--- Models/Pessoas.py
+++ Models/Pessoas.py
@@ -4,24 +4,20 @@ from abc import ABC, abstractmethod
 # Classe abstrata
 class Pessoas(ABC):
     def __init__(self, id, nome, cpf, data_nasc):
-        self._id = id  # Atributo privado
-        self._nome = nome  # Atributo privado
-        self._cpf = cpf # Atributo privado
-        self._data_nasc = data_nasc # Atributo privado
+        self._id = id  
+        self._nome = nome  
+        self._cpf = cpf 
+        self._data_nasc = data_nasc 
  
-    # Método get para o atributo id
     def get_id(self):
         return self._id
 
-    # Método set para o atributo id
     def set_id(self, codigo):
         self._id = codigo
     
-    # Método get para o atributo nome
     def get_nome(self):
         return self._nome
 
-    # Método set para o atributo nome
     def set_nome(self, nome):
         self._nome = nome
     

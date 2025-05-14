a1136b34aaabde785692de8c9705fdf681aba89c
EAn022 <162371161+EAn022@users.noreply.github.com>
2025-05-14 19:18:28 +0000

    fix medicoServices

---
+++ Services/MedicoServices.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conexao = sqlite3.connect('Hospital.db')
+
+cursor = conexao.cursor()
+
+cursor.execute(
+    '''
+        CREATE TABLE medicos(
+            id INTEGER NOT NULL PRIMARY KEY,
+            id_funcionario INTEGER NOT NULL,
+            crm TEXT NOT NULL,
+            especialidade TEXT NOT NULL,  
+            FOREIGN KEY (id_funcionario) REFERENCES funcionarios(id)
+        );
+    '''
+
+);
+
+cursor.close()
+print("Tabela MEDICOS criada com sucesso")

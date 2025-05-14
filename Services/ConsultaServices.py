a2f20a226ab5fac259b0fde2482095f2ec6b7e95
Decryy <enzomodenapro@gmail.com>
2025-05-14 15:42:07 -0300

    Resolving problems

--- Services/ConsultaServices.py
+++ Services/ConsultaServices.py
@@ -10,6 +10,10 @@ cursor.execute(
             id INTEGER PRIMARY KEY,
             data_consulta DATE,
             observacoes TEXT NOT NULL,
+            id_paciente INTEGER NOT NULL,
+            id_medico INTEGER NOT NULL,
+            FOREIGN KEY (id_paciente) REFERENCES pacientes(id),
+            FOREIGN KEY (id_medico) REFERENCES medicos(id)
 
 );
     '''
